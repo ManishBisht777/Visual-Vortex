e611e8d175462cf24f653b97bf1e14e4577dc55c
ManishBisht7 <manish@mydahlia.app>
2023-06-20 21:42:06 +0530

    feat: setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
   return (
-    <main>
-      <h1>Yoooo</h1>
-    </main>
-  );
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <h1>Hi</h1>
+    </section>
+  )
 }
---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Vortex",
+  description: "Generate and play",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
