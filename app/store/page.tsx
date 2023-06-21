f50b7bd24ceaf01831f47fc1efe344f2911701f2
ManishBisht7 <manish@mydahlia.app>
2023-06-21 21:43:29 +0530

    feat: marketplace ui

---
+++ app/store/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div className="container dark:bg-gray-900 bg-slate-100  min-h-screen mt-6 rounded-l-3xl p-6">
+      <h1>store</h1>
+    </div>
+  )
+}
